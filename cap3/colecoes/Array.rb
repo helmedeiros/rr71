449a19c9a0fc2efd11aa11319beea698847fcc1e
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2011-03-26 19:46:59 -0300

    Working with arrays.

---
+++ cap3/colecoes/Array.rb
@@ -0,0 +1,13 @@
+lista = Array.new
+lista << "primeiro"
+lista << "segundo"
+
+puts lista[0]             
+puts lista[1]
+
+lista2 = [1, 2, "string", :simbolos, /$regex^/]
+
+
+puts lista2[2]
+puts lista2[3]
+puts lista2[4]
