7c71aac0374f47b40ff64c91acc607ada97ef54e
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2011-07-07 23:36:31 -0300

    Added comum methods session

---
+++ cap3/3.2/MetodosComuns.rb
@@ -0,0 +1,14 @@
+ONE = 1
+OLA = "OLA"
+UEPA = :UEPA
+OBJ = Object.new()
+
+puts ONE.class
+puts OLA.class
+puts UEPA.class
+puts OBJ.class
+
+puts ONE.to_s()
+puts OLA.to_s()
+puts UEPA.to_s()
+puts OBJ.to_s()
