d92a0a5756bb3c6b01711cf15a6de2c0409a9fa7
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2011-07-17 10:34:03 -0400

    Understood new conventions on ruby name methods

--- cap3/3.15/convencoes.rb
+++ cap3/3.15/convencoes.rb
@@ -3,8 +3,15 @@ texto = "nao sou vazio"
 puts texto.empty?
 
 class Conta
+  def initialize(identificador)
+    @identificador = identificador
+  end
+  
+  attr_accessor :identificador
+    
   #When a method returns a boolean by convention it's finished by ?
-  def estaAtivo?
+  #Method's names by convention need to be "_" separeted instead of CamelCase.
+  def esta_ativo?
     @ativo
   end
   
@@ -17,10 +24,20 @@ class Conta
     @ativo = false
   end
   
+  #By default the equals compartion method compare only equality between references.
+  def ==(outra)
+    self.identificador == outra.identificador
+  end
+    
 end
 
-c = Conta.new
+c = Conta.new(1234)
 c.ativa!
-puts c.estaAtivo?
+puts c.esta_ativo?
 c.cancela!
-puts c.estaAtivo?
+puts c.esta_ativo?
+
+c1 = Conta.new(4567)
+puts c == c1
+c1.identificador = 1234
+puts c == c1
