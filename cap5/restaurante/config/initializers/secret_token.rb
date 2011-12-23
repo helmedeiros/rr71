f1695fcf3fa41f25ec65d833f5701e9ce5d17617
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2011-12-23 08:40:33 -0200

    Initial version

---
+++ cap5/restaurante/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restaurante::Application.config.secret_token = 'd6a388d3096eb7f122d0622c9d9165bf5ddd346dfae0581be63d4d05199b959220d6841e99438bb0af6f3ac04724a9659b9fb675312f968ff4bbc59bcf60ee66'
