0985dc0987f155e45f46e9b137656c55eed0a56d
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2011-12-23 10:40:26 -0200

    Added new fields on the restaurants table;

--- cap5/restaurante/db/migrate/20111223110004_create_restaurants.rb
+++ cap5/restaurante/db/migrate/20111223110004_create_restaurants.rb
@@ -1,8 +1,13 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-
+      t.string :name, :limit => 80
+      t.string :endereco 
       t.timestamps
     end
+  end
+  
+  def self.down
+    drop_table :restaurants
   end
 end
